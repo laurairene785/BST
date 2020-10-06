b01e922d01b528be10fb6618e53bb410f9eef2c4
laurairene785 <49923511+laurairene785@users.noreply.github.com>
2020-10-06 15:48:54 -0400

    Add files via upload

---
+++ BST.java
@@ -0,0 +1,9 @@
+public interface BST {
+    public int getSize();
+    public int getHigh();
+    public void ShowPreorder();
+    public void ShowInorder();
+    public void ShowPostorder();
+   // public void ShowLeveloreder();
+
+}
