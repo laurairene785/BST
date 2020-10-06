509d379099193c90315a336b3497c36ea0285bba
laurairene785 <49923511+laurairene785@users.noreply.github.com>
2020-10-06 16:18:26 -0400

    Delete BST.java

--- BST.java
+++
@@ -1,9 +0,0 @@
-public interface BST {
-    public int getSize();
-    public int getHigh();
-    public void ShowPreorder();
-    public void ShowInorder();
-    public void ShowPostorder();
-   // public void ShowLeveloreder();
-
-}
